ffcc5b47c3a279328b2384914f5e627ba9966edb
Marble <scnideffer@gmail.com>
2023-03-08 11:49:30 -0800

    added query platforms function in main.py

--- CrossPost/main.py
+++ CrossPost/main.py
@@ -4,56 +4,77 @@ from credentials import Credentials
 import tkinter as tk
 import tkinter.filedialog as fd
 
-def create_post():
-    root = tk.Tk()
-    root.withdraw()
+# function to query which platforms app can post to.
+platforms = ["Facebook", "LinkedIn", "Twitter"]
+def query_platforms(platforms):
+    # initialize responses list
+    responses = []
 
-    # prompt user for text submission
-    text_submission = input("Please enter your text submission (5-256 characters): ")
-    while len(text_submission) < 5 or len(text_submission) > 256:
-        text_submission = input("Please enter your text submission (5-256 characters): ")
+    for p in platforms:
+        response = input(f"Would you like CrossPost to post to {p}? (yes/no): ")
+        if response.lower() == 'yes':
+            responses.append(p[:2].lower())
+    return responses
 
-    # prompt user for hashtags
-    add_hashtags = input("Do you want to add hashtags? (Y/N): ")
-    while add_hashtags not in ["Y", "N"]:
-        add_hashtags = input("Do you want to add hashtags? (Y/N): ")
+responses = query_platforms(platforms)
+print(responses)
 
-    hashtags = ""
-    if add_hashtags == "Y":
-        hashtags_input = input("Please enter your hashtags (comma separated): ")
-        hashtags_list = hashtags_input.split(",")
-        for tag in hashtags_list:
-            hashtags += "#" + tag.strip().lower() + " "
 
-    # prompt user to select a picture
-    pic_folder_path = "./CrossPost/pics"
-    pic_filetypes = [("JPEG files", "*.jpg"), ("PNG files", "*.png")]
-    pic_filename = fd.askopenfilename(initialdir=pic_folder_path, title="Select Picture", filetypes=pic_filetypes)
 
-    if not pic_filename:
-        print("No picture selected. Exiting.")
-        return
 
-    # prepare data to hand off to another module
-    data = {
-        "text": text_submission + " " + hashtags,
-        "picture_path": pic_filename
-    }
-    print("\nSUCCESS!! DATA TO BE SENT TO SOCIAL MEDIA PLATFORMS HERE! \n", "Data:", data)
 
-# prompt user for post data
-data = create_post()
 
-# log in to Facebook
-fb = Facebook
-username, password = fb.prompt_login()
-fb.login(username, password)
 
-# post to Facebook
-fb.post_to_facebook(data)
+# def create_post():
+#     root = tk.Tk()
+#     root.withdraw()
 
-# log out
-fb.logout()
+#     # prompt user for text submission
+#     text_submission = input("Please enter your text submission (5-256 characters): ")
+#     while len(text_submission) < 5 or len(text_submission) > 256:
+#         text_submission = input("Please enter your text submission (5-256 characters): ")
+
+#     # prompt user for hashtags
+#     add_hashtags = input("Do you want to add hashtags? (Y/N): ")
+#     while add_hashtags not in ["Y", "N"]:
+#         add_hashtags = input("Do you want to add hashtags? (Y/N): ")
+
+#     hashtags = ""
+#     if add_hashtags == "Y":
+#         hashtags_input = input("Please enter your hashtags (comma separated): ")
+#         hashtags_list = hashtags_input.split(",")
+#         for tag in hashtags_list:
+#             hashtags += "#" + tag.strip().lower() + " "
+
+#     # prompt user to select a picture
+#     pic_folder_path = "./CrossPost/pics"
+#     pic_filetypes = [("JPEG files", "*.jpg"), ("PNG files", "*.png")]
+#     pic_filename = fd.askopenfilename(initialdir=pic_folder_path, title="Select Picture", filetypes=pic_filetypes)
+
+#     if not pic_filename:
+#         print("No picture selected. Exiting.")
+#         return
+
+#     # prepare data to hand off to another module
+#     data = {
+#         "text": text_submission + " " + hashtags,
+#         "picture_path": pic_filename
+#     }
+#     print("\nSUCCESS!! DATA TO BE SENT TO SOCIAL MEDIA PLATFORMS HERE! \n", "Data:", data)
+
+# # prompt user for post data
+# data = create_post()
+
+# # log in to Facebook
+# fb = Facebook
+# username, password = fb.prompt_login()
+# fb.login(username, password)
+
+# # post to Facebook
+# fb.post_to_facebook(data)
+
+# # log out
+# fb.logout()
 
 #log in to twitter
 # tw = Twitter
